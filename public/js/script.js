37dbea73d509c5f47b5db1f1fa6bc431d8e4a621
Roeland <roelandsteevan@gmail.com>
2021-04-13 18:12:52 +0200

    river cards showing

--- public/js/script.js
+++ public/js/script.js
@@ -3,10 +3,16 @@ const button = document.querySelector('#button');
 
 socket.on('serve cards', (cards, river) => {
     const userCardsImg = document.querySelectorAll('.user-card');
+    const riverCardsImg = document.querySelectorAll('.river-card');
 
     userCardsImg.forEach((card, i) => {
         card.setAttribute('src', `${cards.cards[i].image}`)
-    })
+    });
+
+    riverCardsImg.forEach((card, i) => {
+        card.setAttribute('src', `${river.cards[i].image}`)
+    });
+
     console.log('cards:', cards)
     console.log('river:', river)
 });
